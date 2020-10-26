cb78a3cd9e9d8754781c3c3f97c62797542cc0f8
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-10-27 05:29:37 +0700

    setup jwt auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,106 +13,106 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthController extends Controller
 {
 
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
 
-    // public function login(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'number_phone' => 'required|numeric|min:11',
-    //         'password'     => 'required|string'
-    //     ]);
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'number_phone' => 'required|numeric|min:11',
+            'password'     => 'required|string'
+        ]);
 
-    //     if ($validator->fails()) {
-    //         return response()->json(['message' => $validator->errors()], 422);
-    //     }
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 422);
+        }
 
-    //     try {
-    //         if (! $token = JWTAuth::attempt($validator->validated())) {
+        try {
+            if (! $token = JWTAuth::attempt($validator->validated())) {
 
-    //             return response()->json(['error' => "Unauthorized"], 401);
+                return response()->json(['error' => "Unauthorized"], 401);
 
-    //         }
-    //     } catch (JWTException $e) {
+            }
+        } catch (JWTException $e) {
 
-    //         return response()->json(['error' => 'Could Not Create Token!'], 500);
+            return response()->json(['error' => 'Could Not Create Token!'], 500);
 
-    //     }
+        }
 
-    //     return $this->responseWithToken($token);
+        return $this->responseWithToken($token);
 
-    // }
+    }
 
-    // public function register(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'name'         => 'required|string|between:2,100',
-    //         'number_phone' => 'required|numeric|min:11|unique:users',
-    //         'password'     => 'required|string|min:5'
-    //     ]);
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'         => 'required|string|between:2,100',
+            'number_phone' => 'required|numeric|min:11|unique:users',
+            'password'     => 'required|string|min:5'
+        ]);
 
-    //     if ($validator->fails()) {
-    //         return response()->json(['message' => $validator->errors()], 422);
-    //     }
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 422);
+        }
 
-    //     $user = User::create(array_merge($validator->validated(), [
-    //         'password' => bcrypt($request->password)
-    //     ]));
+        $user = User::create(array_merge($validator->validated(), [
+            'password' => bcrypt($request->password)
+        ]));
 
-    //     if ($user) {
-    //         return response()->json([
-    //             'message' => 'User Successfully Registered',
-    //             'data'    => $user,
-    //         ]);
-    //     } else {
-    //         return response()->json([
-    //             'message' => 'User Failed Registered',
-    //             'data'    => '',
-    //         ]);
-    //     }
-    // }
+        if ($user) {
+            return response()->json([
+                'message' => 'User Successfully Registered',
+                'data'    => $user,
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'User Failed Registered',
+                'data'    => '',
+            ]);
+        }
+    }
 
-    // public function bioProfile()
-    // {
-    //     return response()->json(['profile' =>auth()->user()]);
-    // }
+    public function bioProfile()
+    {
+        return response()->json(['profile' =>auth()->user()]);
+    }
 
-    // public function refresh()
-    // {
-    //     return $this->responseWithToken(JWTAuth::refresh());
-    // }
+    public function refresh()
+    {
+        return $this->responseWithToken(JWTAuth::refresh());
+    }
 
-    // public function logout()
-    // {
-    //     JWTAuth::logout();
+    public function logout()
+    {
+        JWTAuth::logout();
 
-    //     return response()->json(['message' => 'User successfully signed out']);
-    // }
+        return response()->json(['message' => 'User successfully signed out']);
+    }
 
-    // public function getAuthenticatedUser()
-    // {
-    //     try {
+    public function getAuthenticatedUser()
+    {
+        try {
 
-    //         if (! $user = JWTAuth::parseToken()->authenticate()) {
-    //             return response()->json(['user_not_found'], 404);
-    //         }
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
 
-    //     } catch (TokenExpiredException $e) {
+        } catch (TokenExpiredException $e) {
 
-    //         return response()->json(['token_expired'], $e->getStatusCode());
+            return response()->json(['token_expired'], $e->getStatusCode());
 
-    //     } catch (TokenInvalidException $e) {
+        } catch (TokenInvalidException $e) {
 
-    //         return response()->json(['token_invalid'], $e->getStatusCode());
+            return response()->json(['token_invalid'], $e->getStatusCode());
 
-    //     } catch (JWTException $e) {
+        } catch (JWTException $e) {
 
-    //         return response()->json(['token_absent'], $e->getStatusCode());
+            return response()->json(['token_absent'], $e->getStatusCode());
 
-    //     }
+        }
 
-    //     return response()->json(['user' => $user]);
-    // }
+        return response()->json(['user' => $user]);
+    }
 }
--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -19,17 +19,17 @@ class JwtMiddleware extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
-        // try {
-        //     $user = JWTAuth::parseToken()->authenticate();
-        // } catch (JWTException $e) {
-        //     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-        //         return response()->json(['status' => 'Token is Invalid']);
-        //     }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-        //         return response()->json(['status' => 'Token is Expired']);
-        //     }else{
-        //         return response()->json(['status' => 'Authorization Token not found']);
-        //     }
-        // }
-        // return $next($request);
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (JWTException $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else{
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+        return $next($request);
     }
 }
